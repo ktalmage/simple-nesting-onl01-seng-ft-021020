2759b83872dc4a442434a314884a9cc2438adc53
Kyle Talmage <ktalmage85@gmail.com>
2020-02-14 19:36:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,7 +129,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+    programmer_hash[:alan_kay][:known_for] = "GUI" 
 end
 
 def adding_to_dennis
@@ -149,5 +149,5 @@ def adding_to_dennis
         }
      }
 
-programmer_hash[:alan_kay][:known_for] = "GUI"
+
 end
