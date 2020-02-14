70ffeb341995dc61bd71dba3e820915d9c8a037c
Kyle Talmage <ktalmage85@gmail.com>
2020-02-14 19:41:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -151,6 +151,6 @@ def adding_to_dennis
      }
 
 
-programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
   return programmer_hash
 end
