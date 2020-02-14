7cf0b5e7aad6e6f520f2e0b5b0c8150477b14dfe
Kyle Talmage <ktalmage85@gmail.com>
2020-02-14 19:30:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,10 +84,9 @@ end
 
 def adding_matz
  programmer_hash[:yukihiro_matsumoto][:known_for] => "Ruby"
- programmer_hash[:yukihiro_matsumoto][:languages] =>
-   :known_for => "Ruby",
-    :languages => ["LISP", "C"]
- }
+ programmer_hash[:yukihiro_matsumoto][:languages] => ["LISP", "C"]
+   
+ 
 
 	programmer_hash = 
  		{
